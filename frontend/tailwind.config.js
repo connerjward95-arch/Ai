cae471fb98de2ab598f38580b02aa66b28c517f9
connerjward95-arch <connerjward95@gmail.com>
2026-06-07 22:22:43 -0700

    Fix Android build: Tailwind/PostCSS setup, Capacitor SystemBars patch, Kotlin stdlib (#1)
    * Fix Android build: Tailwind/PostCSS setup; patch Capacitor SystemBars; force Kotlin stdlib 1.8.22
    
    * Update build.gradle
    
    * Update cordova.variables.gradle
    
    * Update AndroidManifest.xml
    
    * Update config.xml
    
    * Update asset-manifest.json
    
    * Update index.html
    
    * Update main.77a8693b.css.map
    
    * Update main.77a8693b.css
    
    * Update tailwind.config.js
    
    * Update tailwind.config.js
    
    * Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
+    "./public/index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
@@ -18,3 +18,4 @@ module.exports = {
   },
   plugins: [],
 };
+
